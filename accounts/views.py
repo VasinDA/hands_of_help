6dd0ec9fbd70ffb831f59d5266b5936854171697
Denis Vasin <svga.shop@gmail.com>
2022-10-07 14:48:07 +0300

    start working accounts

---
+++ accounts/views.py
@@ -0,0 +1,9 @@
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from .forms import UserRegisterForm
+ 
+class SignUpView(CreateView):
+    form_class = UserRegisterForm
+    success_url = reverse_lazy('login')
+    template_name = "home.html"
+    success_message = "Your profile was created successfully"
