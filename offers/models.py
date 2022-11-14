96faed8df4093d7c74bbcf6cf9fa856f11b2a047
Denis Vasin <svga.shop@gmail.com>
2022-11-14 10:19:37 +0200

    hands of help

--- offers/models.py
+++ offers/models.py
@@ -1,3 +1,18 @@
+from django.conf import settings
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class Offers(models.Model):
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+
+def __str__(self):
+    return self.title
+
+def get_absolute_url(self):
+    return reverse("offers_detail", kwargs=("pk", self.pk))
