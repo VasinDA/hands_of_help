bd0ec7e053f97bf9cec60c43f9effaa120cb1ce9
Denis Vasin <svga.shop@gmail.com>
2023-01-11 16:04:58 +0200

    offers list home

--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,16 @@
 from .forms import ContactForm
+from offers.models import Offers
+from django.views.generic import ListView
 from django.views.generic import TemplateView
 
-class HomePageView(TemplateView):
+class HomePageView(ListView):
+    model = Offers    
     template_name = "home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['offers'] = Offers.objects.all()
+        return context
 
 class AboutPageView(TemplateView):
     template_name = "about.html"
