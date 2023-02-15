40f9b7932b5292d1541ec66ca8c80d41af293ac0
Denis Vasin <svga.shop@gmail.com>
2023-02-15 14:44:28 +0200

    Requests list on home page

--- pages/views.py
+++ pages/views.py
@@ -1,15 +1,17 @@
 from .forms import ContactForm
 from offers.models import Offers
+from requests.models import Requests
 from django.views.generic import ListView
 from django.views.generic import TemplateView
 
 class HomePageView(ListView):
-    model = Offers    
+    model = Offers # why work without model Requests
     template_name = "home.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['offers_list'] = Offers.objects.all().order_by('-date')[:5]
+        context['requests_list'] = Requests.objects.all().order_by('-date')[:5]
         return context
 
 class AboutPageView(TemplateView):
