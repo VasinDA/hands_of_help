6a31ce8b0a959b60437d3947260a8c93c1f59a56
Denis Vasin <svga.shop@gmail.com>
2023-04-03 21:53:10 +0300

    requests

--- requests/forms.py
+++ requests/forms.py
@@ -1,6 +1,5 @@
 from django import forms
 from requests.models import Requests
-from offers.models import Offers
 
 class CreationRequestsForm(forms.ModelForm):
     title = forms.CharField(label='Назва', min_length=10)
@@ -18,8 +17,3 @@ class UpdateRequestsForm(forms.ModelForm):
     class Meta:
         model = Requests
         fields = ['title', 'body']
-
-class CreationOffersForm(forms.ModelForm):
-    class Meta:
-        model = Offers
-        fields = ['title', 'body', 'url_image', 'location']
--- requests/views.py
+++ requests/views.py
@@ -1,10 +1,12 @@
 from django.views import View
-from .forms import CreationRequestsForm, UpdateRequestsForm, CreationOffersForm
+from offers.forms import CreationOffersForm
+from .forms import CreationRequestsForm, UpdateRequestsForm
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic.detail import SingleObjectMixin
 from django.urls import reverse_lazy, reverse
+from offers.models import Offers
 from .models import Requests
 
 class OfferGet(DeleteView):
@@ -14,6 +16,7 @@ class OfferGet(DeleteView):
     def get_context_data(self, **kwargs) :
         context = super().get_context_data(**kwargs)
         context["form"] = CreationOffersForm()
+        context["offers_list"] = Offers.objects.filter(request_id=self.get_object().pk)
         return context
 
 class OfferPost(SingleObjectMixin, FormView):
@@ -33,8 +36,8 @@ class OfferPost(SingleObjectMixin, FormView):
         return super().form_valid(form)
     
     def get_success_url(self):
-        offer = self.get_object() 
-        return reverse("requests_detail", kwargs={"pk": offer.pk})
+        request = self.get_object() 
+        return reverse("requests_detail", kwargs={"pk": request.pk})
 
 class RequestsListView(ListView):
     model = Requests
