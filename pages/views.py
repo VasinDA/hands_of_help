7c1a6e283b351c8674a1ae5d883d2af02b8e0979
Denis Vasin <svga.shop@gmail.com>
2023-05-12 16:09:33 +0300

    final alfa version 1.01 logo contact form

--- pages/views.py
+++ pages/views.py
@@ -3,6 +3,10 @@ from offers.models import Offers
 from requests.models import Requests
 from django.views.generic import ListView
 from django.views.generic import TemplateView
+from django.urls import reverse_lazy
+from django.core.mail import send_mail
+from django.views.generic.edit import FormView
+from django.views.generic.detail import SingleObjectMixin
 
 class HomePageView(ListView):
     model = Offers # why work without Requests model
@@ -17,5 +21,17 @@ class HomePageView(ListView):
 class AboutPageView(TemplateView):
     template_name = "about.html"
 
-class ContactsPageView(TemplateView):
+class ContactsPageView(FormView):
+    form_class = ContactForm
     template_name = "contacts.html"
+    success_url = reverse_lazy("contacts")
+
+    def form_valid(self, form):
+        subject = 'Новое сообщение от {}'.format(form.cleaned_data['first_name'])
+        message = 'От: {}\n\n{}'.format(
+            form.cleaned_data['email'],
+            form.cleaned_data['message'])
+        from_email = 'handsofhelpukraine@gmail.com'
+        recipient_list = ['handsofhelpukraine@gmail.com']
+        send_mail(subject, message, from_email, recipient_list)
+        return super().form_valid(form)
